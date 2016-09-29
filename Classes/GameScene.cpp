c503f6152ef84a47a57f5536154f4af7be3b4ae4
Dimitris Damianidis <teh.jimisdam@gmail.com>
2016-09-29 23:40:10 +0300

    Changer Resources Older Structure

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -24,12 +24,12 @@ bool GameScene::init() {
 	background->drawSolidRect(origin, visibleSize, Color4F(0.5, 0.5, 0.5, 1.0));
 	this->addChild(background);
 
-    feet = Sprite::create("feet.png");
+    feet = Sprite::create("images/feet.png");
     feet->setScale(1);
     feet->setPosition(Vec2(visibleSize.width / 2, feet->getContentSize().height / 2));
     this->addChild(feet);
 
-    pause = Sprite::create("pause.png");
+    pause = Sprite::create("images/pause.png");
     pause->setScale(1);
 	pause->setPosition(Vec2(visibleSize.width - pause->getContentSize().width, pause->getContentSize().height));
 	this->addChild(pause);
