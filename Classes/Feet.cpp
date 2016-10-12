29cf63db1edd3b0581d5bf2818f4072776c0eee9
Antony Skarlatos <hepic135@gmail.com>
2016-10-12 12:59:53 +0300

    Add animation to the Feet Player

--- Classes/Feet.cpp
+++ Classes/Feet.cpp
@@ -6,10 +6,25 @@ Feet::Feet(cocos2d::Layer *layer) {
 	visibleSize = Director::getInstance()->getVisibleSize();
 	origin = Director::getInstance()->getVisibleOrigin();
 
-	feetSprite = Sprite::create("images/feet.png");
+	feetSprite = Sprite::create("images/feet_l1.png");
 	feetSprite->setScale(1);
 	feetSprite->setPosition(Vec2(visibleSize.width / 2, feetSprite->getContentSize().height / 2));
 	layer->addChild(feetSprite);
+
+	Vector<SpriteFrame*> animFrames(15);
+	char str[100] = { 0 };
+
+	for (int i = 1; i <= 6; ++i)
+	{
+		sprintf(str, "images/feet_%d.png", i);
+		auto frame = SpriteFrame::create(str, Rect(0, 0, feetSprite->getContentSize().width, feetSprite->getContentSize().height));
+		animFrames.pushBack(frame);
+	}
+
+	auto animation = Animation::createWithSpriteFrames(animFrames, 0.1f);
+	auto animate = RepeatForever::create(Animate::create(animation));
+
+	feetSprite->runAction(animate);
 }
 
 void Feet::Move(cocos2d::Vec2 touch_location) {
@@ -37,8 +52,8 @@ void Feet::Move(cocos2d::Vec2 touch_location) {
 		rotate = RotateTo::create(time, -slope);
 	}
 
-	auto action = Spawn::createWithTwoActions(move, rotate);
-	action->setTag(1);
+	auto move_action = Spawn::createWithTwoActions(move, rotate);
+	move_action->setTag(1);
 
-	feetSprite->runAction(action);
+	feetSprite->runAction(move_action);
 }
--- Classes/MenuScene.h
+++ Classes/MenuScene.h
@@ -5,7 +5,6 @@
 
 class MainMenuScene : public cocos2d::Layer
 {
-
 public:
     static cocos2d::Scene* createScene();
     virtual bool init();
